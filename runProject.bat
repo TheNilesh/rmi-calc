d27e137c328fda0c5f6d0de2e19b6378b76f85bc
niLesh Akhade <TheNilesh@users.noreply.github.com>
2015-05-27 19:16:13 +0530

    Repaired
    rmic missed

--- runProject.bat
+++ runProject.bat
@@ -1,7 +1,10 @@
 @echo off
 javac CalcServer.java
+rmic CalcServer
 javac CalcClient.java
+rmic CalcClient
 start rmiregistry
+TIMEOUT /T 5 /NOBREAK
 start cmd /k java CalcServer calc
 TIMEOUT /T 5 /NOBREAK
 start cmd /k java CalcClient rmi://localhost/calc
